dd638e606c799a7d02d5843ba943f7b3dcb110cd
shraman <pkpaulpostdn@gmail.com>
2023-07-21 07:59:21 +0530

    added the tailwind file configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
 
-function App() {
+import React from 'react';
+import MyComponent from './MyComponent';
+import './index.css';
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, React App!</h1>
+      <MyComponent /> {/* Use the MyComponent here */}
     </div>
   );
-}
+};
 
 export default App;
