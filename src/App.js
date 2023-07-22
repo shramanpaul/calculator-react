e502017dfafbf6cf710a0eeb1a7a197812b0f143
shraman <22bcs229@iiitdmj.ac.in>
2023-07-22 05:36:36 +0530

    resolving errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 // src/App.js
 
 import React from 'react';
-import MyComponent from './MyComponent';
 import './index.css';
 import Calculator from './Calculator';
 const App = () => {
   return (
     <>
       <Calculator/>
-      {/* <MyComponent /> Use the MyComponent here */}
-    </>
+   </>
   );
 };
 
